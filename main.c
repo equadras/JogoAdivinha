10a62ebff4a5b40cda91c8c3e127f79c0d6befba
Leonardo Muniz <leonardonm15@gmail.com>
2022-06-28 13:55:27 +0000

    ajustes leves

---
+++ main.c
@@ -0,0 +1,48 @@
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int func1(i){
+  int i2 = i;
+  printf("Essa e a funcao %d\n\n ela n faz nada mas poderia fazer :)\n\n", i2);
+  return i2;
+}
+
+int func2(i){
+  int i3 = i;
+  printf("Essa e a funcao %d\n\n", i3);
+  return i3;
+}
+
+int func3(i){
+  int i4 = i;
+  printf("Essa e a funcao %d\n\n", i4);
+  return i4;
+}
+
+int main() {
+  int i;
+  printf("Qual funcao vc deseja executar?\n");
+  scanf("%d", &i);
+  if(i == 1){
+    i = func1(i);
+    printf("A funcao executada foi %d\n", i);
+      return EXIT_SUCCESS;
+  } if(i == 2){
+      i = func2(i);
+      printf("A funcao executada foi %d\n", i);
+    return EXIT_SUCCESS;
+    } if(i == 3){
+      i = func3(i);
+      printf("A funcao executada foi %d\n", i);
+      return EXIT_SUCCESS;
+    } else {
+     printf("Erro!\n");
+    return EXIT_SUCCESS;
+    }
+  
+  return EXIT_SUCCESS;
+}
+
+*/
