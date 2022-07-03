adbf1d05a3f4e95eeb406553531b7a8d58de1e12
leonardonm15 <leonardonm15@gmail.com>
2022-07-03 00:41:52 +0000

    finalização do jogo

--- main.c
+++ main.c
@@ -1,48 +1,98 @@
-
-/*
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int func1(i){
-  int i2 = i;
-  printf("Essa e a funcao %d\n\n ela n faz nada mas poderia fazer :)\n\n", i2);
-  return i2;
+int numero_jogos = 0;
+int historico[5];
+
+int sorteador(){
+  srand(time(NULL));
+  return rand() % 100 + 1;  
 }
 
-int func2(i){
-  int i3 = i;
-  printf("Essa e a funcao %d\n\n", i3);
-  return i3;
+void limpar_hist(){
+  if (numero_jogos == 0){
+    printf("\n-----NAO TEMOS JOGOS PARA TIRAR-----\n");
+    return;
+  }
+  for(int i=0; i <= 4; i ++){
+    historico[i] = 0;
+  }
+  numero_jogos = 0;
+  printf("\n-----HISTÓRICO LIMPO-----\n");
 }
 
-int func3(i){
-  int i4 = i;
-  printf("Essa e a funcao %d\n\n", i4);
-  return i4;
-}
-
-int main() {
-  int i;
-  printf("Qual funcao vc deseja executar?\n");
-  scanf("%d", &i);
-  if(i == 1){
-    i = func1(i);
-    printf("A funcao executada foi %d\n", i);
-      return EXIT_SUCCESS;
-  } if(i == 2){
-      i = func2(i);
-      printf("A funcao executada foi %d\n", i);
-    return EXIT_SUCCESS;
-    } if(i == 3){
-      i = func3(i);
-      printf("A funcao executada foi %d\n", i);
-      return EXIT_SUCCESS;
-    } else {
-     printf("Erro!\n");
-    return EXIT_SUCCESS;
+void mostrar_historico(){
+  if (numero_jogos == 0){
+    printf("\n-----NAO TEMOS JOGOS PARA MOSTRAR-----\n");
+    return;
+  }
+  printf("\n-----HISTORICO DOS UTLIMOS 5 JOGOS-----\n");
+  for (int i = 0; i < 5; i++) {
+    printf("%d - tentativas %d\n", i + 1, historico[i]);
+    if (i == numero_jogos - 1) {
+      return;
     }
-  
-  return EXIT_SUCCESS;
+  }
 }
 
-*/
+int jogo(){
+  int numero_sorteado = sorteador();
+  int tentativa = 0; //numero de tentativas
+  int escolha; //numero escolhido pelo usuario
+  
+  while (tentativa != 10) {  
+    if (tentativa == 0) {
+      printf("\nA maquina ja escolheu o numero dela, sera que vc consegue acertar ?\n");
+      }
+      printf("sua escolha -> ");
+      scanf("%d", &escolha);
+
+    if (escolha > numero_sorteado) {
+      tentativa++;
+      printf("%d é maior que o numero escolhido pela maquina, te restam %d vidas\n", escolha, (10 - tentativa));
+    }
+    if (escolha < numero_sorteado) {
+      tentativa++;
+      printf("%d é menor que o numero escolhido pela maquina, te restam %d vidas\n", escolha, (10 - tentativa));
+    }
+    if (escolha == numero_sorteado) {
+      printf("%d é o numero escolhido pela maquina, VOCE VENCEU PAULO TORRENS\n", escolha);
+      break;
+    }
+  }
+  
+  if(tentativa == 10) {
+    printf("VOCE PERDEU BOBAO, ERA O NUMERO %d\n", numero_sorteado); 
+  }
+  
+  numero_jogos++;
+
+  if (numero_jogos > 5) {
+  historico[0] = historico[1];
+  historico[1] = historico[2];
+  historico[2] = historico[3];
+  historico[3] = historico[4];  
+  }
+  historico[numero_jogos - 1] = tentativa;
+
+  return 1;
+}
+    
+
+int main(){
+  
+  while(1){
+  int opcao;
+  printf("\n1-> Novo jogo\n2-> Ver ultimos 5 jogos\n3-> Limpar historico\n4-> Sair\nSua opção -> ");
+  scanf("%d", &opcao);
+
+  switch(opcao){
+    case 1:{printf("\n-----INICIANDO O JOGO-----\n"); jogo(); break;};
+    case 2:{mostrar_historico(); break;};
+    case 3:{limpar_hist(); break;};
+    case 4:{printf("desligando...\n"); return EXIT_SUCCESS;};
+    default:{printf("Ocorreu um erro Tente novamente\n");}
+  }
+}
+}
